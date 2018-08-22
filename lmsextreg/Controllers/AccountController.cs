f2cdfbb41470e9f929c1e5b59627604b60567a0c
LeeTrent <Lee.Trent.1@gmail.com>
2018-08-22 10:39:08 -0500

    Adds event log functionality to 'Logout' use case

--- lmsextreg/Controllers/AccountController.cs
+++ lmsextreg/Controllers/AccountController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using lmsextreg.Data;
+using lmsextreg.Constants;
+using lmsextreg.Services;
 
 namespace lmsextreg.Controllers
 {
@@ -14,17 +16,33 @@ namespace lmsextreg.Controllers
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger _logger;
+        private readonly IEventLogService _eventLogService;
 
-        public AccountController(SignInManager<ApplicationUser> signInManager, ILogger<AccountController> logger)
+        public AccountController
+        (
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<AccountController> logger,
+            IEventLogService eventLogSvc
+        )
         {
             _signInManager = signInManager;
             _logger = logger;
+            _eventLogService = eventLogSvc;
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Logout()
         {
+            var user = _signInManager.UserManager.GetUserAsync(User).Result;
+            if (user != null)
+            {
+                ///////////////////////////////////////////////////////////////////
+                // Log the 'LOGOUT' event
+                ///////////////////////////////////////////////////////////////////
+                _eventLogService.LogEvent(EventTypeCodeConstants.LOGOUT, user);   
+            }            
+
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
             return RedirectToPage("/Index");
