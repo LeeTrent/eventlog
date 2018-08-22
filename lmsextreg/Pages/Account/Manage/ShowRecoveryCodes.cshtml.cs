9184de81e995a598042332150207637ea4e9ccf2
LeeTrent <Lee.Trent.1@gmail.com>
2018-08-22 10:03:03 -0500

    Adds event log functionality to 'Show Recovery Codes' use case

--- lmsextreg/Pages/Account/Manage/ShowRecoveryCodes.cshtml.cs
+++ lmsextreg/Pages/Account/Manage/ShowRecoveryCodes.cshtml.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using lmsextreg.Constants;
+using lmsextreg.Data;
+using lmsextreg.Services;
 
 namespace lmsextreg.Pages.Account.Manage
 {
     public class ShowRecoveryCodesModel : PageModel
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IEventLogService _eventLogService;
+
+        public ShowRecoveryCodesModel
+        (
+            UserManager<ApplicationUser> userManager,
+            IEventLogService eventLogSvc
+        )
+        {
+            _userManager        = userManager;
+            _eventLogService    = eventLogSvc;
+        }
+
         public string[] RecoveryCodes { get; private set; }
 
         public IActionResult OnGet()
@@ -18,6 +35,16 @@ namespace lmsextreg.Pages.Account.Manage
             {
                 return RedirectToPage("TwoFactorAuthentication");
             }
+  
+            var user = _userManager.GetUserAsync(User).Result;
+            if (user == null)
+            {
+                throw new ApplicationException($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }            
+            ///////////////////////////////////////////////////////////////////
+            // Log the 'SHOW_RECOVERY_CODES' event
+            ///////////////////////////////////////////////////////////////////
+            _eventLogService.LogEvent(EventTypeCodeConstants.SHOW_RECOVERY_CODES, user);                  
 
             return Page();
         }
